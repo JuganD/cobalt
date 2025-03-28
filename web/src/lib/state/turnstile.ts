d9c9ff972d5281ce687449128d42655ba0499190
JuganD <40771401+JuganD@users.noreply.github.com>
2025-03-28 20:12:05 +0200

    Update turnstile.ts

--- web/src/lib/state/turnstile.ts
+++ web/src/lib/state/turnstile.ts
@@ -2,16 +2,8 @@ import settings from "$lib/state/settings";
 import cachedInfo from "$lib/state/server-info";
 import { derived, writable } from "svelte/store";
 
-export const turnstileSolved = writable(false);
-export const turnstileCreated = writable(false);
+export const turnstileSolved = true;
+export const turnstileCreated = true;
 
-export const turnstileEnabled = derived(
-    [settings, cachedInfo],
-    ([$settings, $cachedInfo]) => {
-        return !!$cachedInfo?.info?.cobalt?.turnstileSitekey &&
-            !(
-                $settings.processing.enableCustomApiKey &&
-                $settings.processing.customApiKey.length > 0
-            )
-    }
+export const turnstileEnabled = true;
 )
